399657e283489cd109ae80f0c150aaff3abb8521
Ben Edwards <ben.edwards@clock.co.uk>
2016-04-04 14:22:35 +0100

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd _output
+git init
+git config user.name "Travis CI"
+git config user.email "travis@travis-ci.com"
+git add .
+git commit -m "Deploy to GitHub Pages via Travis"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
