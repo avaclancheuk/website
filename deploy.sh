3da5f263d906ea04e8e61c2debeee9c429735097
Ben Edwards <ben.edwards@clock.co.uk>
2016-04-04 14:40:33 +0100

    > Attempt to auto-deploy

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ git config user.name "Travis CI"
 git config user.email "travis@travis-ci.com"
 git add .
 git commit -m "Deploy to GitHub Pages via Travis"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
