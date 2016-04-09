72078eaefe330fc257886f63435829db34a57419
Ben Edwards <hello@benedfit.com>
2016-04-09 12:21:40 +0100

    > Fixes lint errors

--- pliers/imagemin.js
+++ pliers/imagemin.js
@@ -2,11 +2,11 @@ module.exports = createTask
 
 var pliersImagemin = require('pliers-imagemin')
 
-function createTask(pliers) {
+function createTask (pliers) {
 
   pliers('imagemin', function (done) {
 
-    pliersImagemin(pliers, pliers.filesets.images)(function(err) {
+    pliersImagemin(pliers, pliers.filesets.images)(function (err) {
       if (err) return done(err)
       done()
     })
