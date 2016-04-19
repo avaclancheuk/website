8920109493ea928695afb55b63ae97cd601998b5
Ben Edwards <ben.edwards@clock.co.uk>
2016-04-19 16:41:37 +0100

    > Rename build script

--- pliers/build.js
+++ pliers/build.js
@@ -1,15 +1,10 @@
 module.exports = createTask
 
-var harp = require('harp')
+function createTask (pliers) {
 
-function createTask (pliers, config) {
-
-  pliers('build', function (done) {
-
-    var path = __dirname + '/../'
-
-    harp.compile(path + config.src, path + config.dest, done)
-
-  })
+  pliers('build'
+    , 'buildFavicon'
+    , 'buildHtml'
+    )
 
 }
