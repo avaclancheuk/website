bb0a8b36a86d934a6fcb8ca6c2edc83103443e0a
Ben Edwards <ben.edwards@clock.co.uk>
2016-07-05 13:22:16 +0100

    Updates favicon

--- pliers/filesets/images.js
+++ pliers/filesets/images.js
@@ -2,6 +2,7 @@ module.exports = createFileset
 
 function createFileset (pliers, config) {
 
-  pliers.filesets('images', __dirname + '/../../' + config.src + '/img/**/*.{gif,jpg,jpeg,png,svg}')
+  pliers.filesets('images', __dirname + '/../../' + config.src + '/img/**/*.{gif,jpg,jpeg,png,svg}',
+    [ __dirname + '/../../' + config.src + '/img/**/logo__text.svg' ])
 
 }
