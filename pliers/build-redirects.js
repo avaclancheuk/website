ef6d239441a31872257a603f3773cbc6f2ac1104
Ben Edwards <hello@benedfit.com>
2020-11-04 18:18:19 +0000

    feat(redirects): adds PC Discord link

--- pliers/build-redirects.js
+++ pliers/build-redirects.js
@@ -9,7 +9,8 @@ function createTask (pliers, config) {
     var path = __dirname + '/../'
       , redirectsFile = path + config.dest + '/_redirects'
       , redirects =
-        [ 'https://discord.avaclanche.uk/* https://discordapp.com/invite/GSdr2t9 301!'
+        [ 'https://discord.avaclanche.uk/pc https://discordapp.com/invite/Fc3AKYD 301!'
+        , 'https://discord.avaclanche.uk/* https://discordapp.com/invite/GSdr2t9 301!'
         , 'https://forum.avaclanche.uk/* https://avaclanche.uk 301!'
         , 'https://slack.avaclanche.uk/* https://avaclanche.uk 301!'
         ]
